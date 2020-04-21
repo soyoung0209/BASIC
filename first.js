67cad2d7577f29ad3d175ee9b2a86cbadb09d85d
SOYOUNG PARK <soyoungpark@SOYOUNGui-MacBookPro.local>
2020-04-21 20:34:47 +0900

    first class

---
+++ first.js
@@ -0,0 +1,98 @@
+function testtest() {
+   /** *var test = '123';
+var myname = '박소영';
+var myage = 26;
+
+    console.log(myage,myname);
+    console.log(test);*/
+
+    /**  var myboyfriend = null;
+    console.log(myboyfriend);*/
+
+    /**var iscute = true
+    var isrich = false
+    console.log(iscute,isrich);**/
+    
+    /**var furith = 'apple';
+    if(furith === 'strabarry'){
+        console.log(false)
+    } else if(furith === 'banana'){
+        console.log(false)
+    }else if(furith === 'mangp'){
+        console.log(false)
+    }else if(furith === 'graph'){
+        console.log(false)
+    }else()
+    console.log(true);*/
+
+
+    /**var krong = 'nogunghwan';
+    console.log(krong)
+
+var ourage = 28+26;
+console.log(ourage)
+
+var mydream = null;
+console.log(mydream)
+
+var job;
+console.log(job);
+
+
+
+var krongage = 28
+if (krongage === 30){
+    console.log("아니다");
+  }  else if(krongage === 27){
+      console.log("no");
+  }else if(krongage === 28){
+  console.log("yes")
+  }else{
+      console.log('no')
+  }
+
+
+
+
+
+
+
+   
+}
+testtest();*/
+
+/*** var x = "xxxx"
+ * var는 변수다. 변수값을 만들어서 그안에 값을 할당한다.
+ * 
+ * 변수값 안에는
+ * 1.타입 
+ * -원시값 : string, number, null ,undified, boolean , if, else 가 있다.
+ * -참조값 :object, array,function
+ * 
+ * 
+ * string : 문자
+ * null, undified - 값이 없다
+ * null-값이 없음을 정의 했음
+ * undified-값이 없음을 정의하지 않았음 //에러
+ * 
+ * Boolean : true , false
+ * var isFat = true
+ * var isTall =false
+ * 
+ * 함수 선언
+ * function 0000()
+ * 
+ * if문 예시
+ * if(girfriend === "aaaaa"){
+ * console.log("ture");
+ * }else if (girfriend ==="ppppaaass"){
+ * consol.log("no");
+ * }else if (girfreind===sss){
+ * consol.log("noo")
+ * }else()
+ * console.log(아니다);
+ * }
+ * 
+ */
+ }
+ 
---
+++ for.js
@@ -0,0 +1,15 @@
+function fortest() {
+ var j = ""
+  for (var i = 0; i < 10; i++){
+    console.log (j += "*")
+  }
+} 
+
+fortest();
+
+/** += 뒤에꺼를 앞에다 더한다.
+ * *= 뒤에꺼를 앞에다 곱한다.
+ */
+
+ 
+    
